84df3dc1d8b7714c7c372e83b37b5514a5b1a205
hotakus <ttowfive@gmail.com>
2023-07-05 10:20:50 +0800

    chore(cmake): version

--- version.h
+++ version.h
@@ -2,10 +2,10 @@
 #define INCLUDE_GUARD
 
 #define HHTL_NAME "hhtl"
-#define HHTL_VER  "1.0.0.0"
+#define HHTL_VER  "1.0.1.0"
 #define HHTL_VER_MAJOR "1"
 #define HHTL_VER_MINOR "0"
-#define HHTL_VER_PATCH "0"
+#define HHTL_VER_PATCH "1"
 #define HHTL_VER_TWEAK "0"
 
 #define HHTL_DESC "Hotakus' Hash Table Lite"
