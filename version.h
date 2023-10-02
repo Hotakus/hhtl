f8e89fd6204ff5d658268a66d342a23c74fbe7a3
hotakus <ttowfive@gmail.com>
2023-10-02 18:13:51 +0100

    version update

--- version.h
+++ version.h
@@ -2,11 +2,11 @@
 #define INCLUDE_GUARD
 
 #define HHTL_NAME "hhtl"
-#define HHTL_VER  "1.0.3.0"
+#define HHTL_VER  "1.0.3.1"
 #define HHTL_VER_MAJOR "1"
 #define HHTL_VER_MINOR "0"
 #define HHTL_VER_PATCH "3"
-#define HHTL_VER_TWEAK "0"
+#define HHTL_VER_TWEAK "1"
 
 #define HHTL_DESC "Hotakus' Hash Table Lite"
 #define HHTL_HOMEPAGE_URL "https://github.com/Hotakus/hhtl"
